2c5985f01fed6dd8ec37f9c86988b4f5d600d3fe
KoldenAxelson <KoldenAxelson896@Protonmail.com>
2026-02-10 17:58:05 -0800

    Telescope local only

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    ...app()->environment("local")
+        ? [App\Providers\TelescopeServiceProvider::class]
+        : [],
 ];
